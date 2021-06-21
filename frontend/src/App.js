435656c2fa0089e38b819b2bce395bc247381916
youjin Cho <ud071233@gmail.com>
2021-06-21 21:06:49 +0900

    #4.2 Styling the Movies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import axios from 'axios';
 import Movie from './Movie';
+import "./App.css";
 
 class App extends Component {
   state = {
@@ -18,11 +19,25 @@ class App extends Component {
   render() {
     const {isLoading, movies} = this.state;
     return (
-      <div>
-        {isLoading ? "Loading..." : movies.map(movie => (
-        <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
-        ))}
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+              <span class="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
